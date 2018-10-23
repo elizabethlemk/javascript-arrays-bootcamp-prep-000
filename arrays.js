8659ebeec0d543384757596ad5e065eb2dc3f945
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-10-23 21:35:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,7 @@ var chocolateBars = [
     'skittles'
   ]
 
+<<<<<<< HEAD
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift('foo')
   return array
@@ -47,4 +48,30 @@ function destructivelyRemoveElementFromEndOfArray(array){
 function removeElementFromEndOfArray(array){
   array = array.slice(0, array.length - 1)
   return array
+=======
+
+
+function addElementToBeginningOfArray(array, element){
+  var array = [1]
+  array.unshift('foo');
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element){
+  const array = [1]
+  const newarray = ['foo',...array];
+  return newarray
+}
+
+function addElementToEndOfArray(array, element){
+  var array = [1]
+  array.push('foo');
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  const array = [1]
+  const newarray = [...array, 'foo']
+  return newarray
+>>>>>>> b6fd6abcdc20f6d726611e2cf7c2e1e0c4e68d5f
 }
